8d92ad82f762e77b069a65ba6ebc15dac83c5725
Kevin Roark <ker2143@columbia.edu>
2015-05-02 11:52:51 -0400

    foot videos

---
+++ convert_mp4.sh
@@ -0,0 +1,3 @@
+~/code/art/shane/mistershane/webm.sh video/rotating_foot.mp4 video/rotating_foot.webm
+~/code/art/shane/mistershane/webm.sh video/foot_bone.mp4 video/foot_bone.webm
+~/code/art/shane/mistershane/webm.sh video/foot_massage.mp4 video/foot_massage.webm
