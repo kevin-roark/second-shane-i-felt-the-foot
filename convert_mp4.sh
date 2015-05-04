f289c548de15812dc2f0cdd18bd27f81ea48b250
Kevin Roark <ker2143@columbia.edu>
2015-05-03 22:38:19 -0400

    replace foot bone with foot slap

--- convert_mp4.sh
+++ convert_mp4.sh
@@ -1,5 +1,5 @@
 ~/code/art/shane/mistershane/webm.sh video/rotating_foot.mp4 video/rotating_foot.webm
-~/code/art/shane/mistershane/webm.sh video/foot_bone.mp4 video/foot_bone.webm
+~/code/art/shane/mistershane/webm.sh video/footslap.mp4 video/footslap.webm
 ~/code/art/shane/mistershane/webm.sh video/foot_massage.mp4 video/foot_massage.webm
 ~/code/art/shane/mistershane/webm.sh video/sean.mp4 video/sean.webm
 ~/code/art/shane/mistershane/webm.sh video/kevin.mp4 video/kevin.webm
